c617f310809479d8faa0bd1aba7ed35e2d938ab5
brendasegal <bsegal26@gmail.com>
2015-01-22 18:53:45 +0000

    adding home and help pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,5 +5,14 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :home
     assert_response :success
   end
-
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+  
+  test "should get help" do
+    get :help
+    assert_response :success
+  end
 end
