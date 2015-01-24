04be80a56bfbaac5b716abd116bb737f2bf4e60c
brendasegal <bsegal26@gmail.com>
2015-01-24 19:36:14 +0000

    added a bunch of contact rquest processing

--- app/controllers/contact_requests_controller.rb
+++ app/controllers/contact_requests_controller.rb
@@ -7,4 +7,19 @@ class ContactRequestsController < ApplicationController
   def new
     @cr = ContactRequest.new
   end
+  
+  def create
+    @cr = ContactRequest.new(contactrequest_params)
+    if @cr.save
+      flash[:success] = "We have received your request and will respond as soon as possible."
+      redirect_to @cr
+    else
+      render 'new'
+    end
+  end
+  
+  private
+    def contactrequest_params
+      params.require(:contact_request).permit(:name, :email, :message)
+    end
 end
